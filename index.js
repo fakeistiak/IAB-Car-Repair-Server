421fccbf0e3b818c0db342bb15a97c6c1432fe18
fakeistiak <istiakahmedb100.com>
2023-10-31 22:31:35 +0600

    delete is working

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ async function run() {
           const query = { _id: new ObjectId(id) }
 
           const options = {
-              projection: { title: 1, price: 1,service_id:1 }
+              projection: { title: 1, price: 1,service_id:1,img:1 }
           };
           const result = await serviceCollection.findOne(query,options);
           res.send(result);
@@ -70,8 +70,15 @@ async function run() {
           console.log(booking);
           const result = await bookingCollection.insertOne(booking);
           res.send(result);
-          
       });
+
+
+      app.delete('/bookings/:id', async (req, res) => {
+          const id = req.params.id;
+          const query ={_id: new ObjectId(id)}
+          const result = await bookingCollection.deleteOne(query)
+          res.send(result);
+      })
 
 
     // Send a ping to confirm a successful connection
