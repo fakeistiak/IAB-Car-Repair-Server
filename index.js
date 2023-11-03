85e166e75a2f02064b637252352e666a021e0753
fakeistiak <istiakahmedb100.com>
2023-11-03 14:32:37 +0600

    jwt token receiving

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 const { ObjectId } = require("mongodb");
+
+
 //MIDDLEWARE
+
+
 app.use(cors({
       origin: ["http://localhost:5173"],
       credentials:true
@@ -28,6 +32,33 @@ const client = new MongoClient(uri, {
   },
 });
 
+// middlewares
+
+const logger = async (req, res, next) => {
+    console.log('called',req.host,req.originalUrl);
+    next()
+}
+const verifyToken = async (req, res, next) => {
+    const token = req.cookies?.token;
+    console.log('value of token in middleware');
+    
+    if (!token) {
+        return res.status(401).send({message:'not authorized'})
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if(err){
+            console.log(err);
+            
+            return res.status(401).send({message:'unauthorized'})
+        }
+        console.log('value in the token', decoded)
+        req.user = decoded;
+          next();
+    });
+  
+}
+
+
 
 async function run() {
   try {
@@ -39,23 +70,26 @@ async function run() {
       
       //   auth related api 
 
-      app.post('/jwt', async(req,res) => {
+      app.post('/jwt',logger, async(req,res) => {
           const user = req.body;
           console.log(user);
           const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-            expiresIn: "1h",
+            expiresIn: "3h",
           });
-          res
-              .cookie('token', token, {
+          res.cookie('token', token, {
                   httpOnly: true,
                   secure: false,
               })
             .send({success:true})
       })
 
-    //   services related api 
 
-    app.get("/services", async (req, res) => {
+
+    //   services related api
+
+      
+      
+    app.get("/services",logger, async (req, res) => {
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
       res.send(result);
@@ -72,11 +106,17 @@ async function run() {
       res.send(result);
     });
 
-    // bookings
+      
+      
+      // bookings
+      
 
-    app.get("/bookings", async (req, res) => {
+
+    app.get("/bookings",logger,verifyToken, async (req, res) => {
         console.log(req.query.email);
-        console.log('tokens',req.cookies.token);
+        console.log('user in the valid token',req.user);
+        
+        // console.log('tokens',req.cookies.token);
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email };
